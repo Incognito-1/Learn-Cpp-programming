8363e432f374f297efd4e0321dd46399a9fab29c
Ihmanuel <114946355+Incognito-1@users.noreply.github.com>
2026-07-03 17:52:15 +0200

    Add files via upload

---
+++ C++ exercise answers/ex1.4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+
+    int x, y, xy;
+    x = 15;
+    y = 4;
+   xy = x*y;
+
+    std::cout<<"The product of"<<" " << x <<" "<< "and" << " "<< y <<" "
+    << "is" <<" "<<xy;
+
+    return 0;
+}
